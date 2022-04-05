653cc97bd7c43c9717382f23a3376a3329416197
spaenleh <basile.1358@gmail.com>
2022-04-05 11:51:28 +0200

    feat: add utils

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { default } from './plugin';
 export { ActionTaskManager } from './task-manager';
 export { ActionService } from './db-service';
 export { BaseAction } from './services/action/base-action';
+export * from './utils/actions';
 export { ActionHandlerInput, ActionHandler } from './types';
---
+++ src/utils/actions.ts
@@ -0,0 +1,11 @@
+import { CLIENT_HOSTS } from '../../test/constants';
+import { VIEW_UNKNOWN_NAME } from '../constants/constants';
+import geoip from 'geoip-lite';
+
+const getGeolocationIp = (ip: string | number): geoip.Lookup =>  geoip.lookup(ip);
+const getView = (headers: {origin?: string | string[]}): string => CLIENT_HOSTS.find(({ hostname: thisHN }) => headers?.origin?.includes(thisHN))?.name ?? VIEW_UNKNOWN_NAME;
+
+export {
+  getGeolocationIp,
+  getView,
+};
